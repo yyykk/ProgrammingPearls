1f6eb6da679bc96f2ccbf71dc5242997bb6ba115
yyykk <yukaieecsu@gmail.com>
2017-04-24 17:46:29 +0800

    20170424

---
+++ 20170424/字符串.cpp
@@ -0,0 +1,28 @@
+﻿/*
+请实现一个函数，将一个字符串中的空格替换成“%20”。例如，当字符串为We Are Happy.则经过替换之后的字符串为We%20Are%20Happy。
+*/
+
+class Solution {
+public:
+	void replaceSpace(char *&str,int length) {
+		int n = 0;
+        for(int i = 0; i < length; ++i){
+            if(str[i] == ' ') ++n;
+        }
+        char *result = new char[length + n * 2];
+        int j = 0;
+        for(int i = 0; i < strlen(str); ++i){
+            if(str[i] != ' '){
+                result[j] = str[i];
+                j += 1;
+            }else{
+                result[j] = '%';
+                result[j + 1] = '2';
+                result[j + 2] = '0';
+            	j += 3;
+            }
+        }
+        result[length + n * 2 - 1] = '\0';
+        str = result;
+    }
+};
---
+++ 20170424/查找.cpp
@@ -0,0 +1,34 @@
+﻿/*
+在一个二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+*/
+
+
+class Solution {
+public:
+    //暴力查找
+    bool Find1(int target, vector<vector<int> > array) {
+        for (int i = 0; i < array.size(); ++i){
+            for (int j  = 0; j < array[i].size(); ++j){
+                if(array[i][j] == target){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    //类二分查找
+    bool Find2(int target, vector<vector<int> > array) {
+        int i = array.size() - 1, j = 0;
+        while(1){
+            if(i < 0 || j == array[i].size()){
+                return false;
+            }else if (target == array[i][j]){
+                return true;
+            }else if (target < array[i][j]){
+                i--;
+            }else if (target > array[i][j]){
+                j++;
+            }
+        }
+    }
+};
